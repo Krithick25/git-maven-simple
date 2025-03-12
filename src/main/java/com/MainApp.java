74f480feea2ceff25ee1a4bdca8dcad1cc3c4dfe
Krithick25 <krithickroshan2504@gmail.com>
2025-03-12 12:57:32 +0530

    second commit

--- src/main/java/com/MainApp.java
+++ src/main/java/com/MainApp.java
@@ -1,5 +1,16 @@
 package com;
 
 public class MainApp {
-
+	
+	public int addition(int a, int b) {
+		return a + b;
+	}
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainApp main = new MainApp();
+		System.out.println(main.addition(10, 10));
+	}
 }
